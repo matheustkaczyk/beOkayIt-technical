e82f5305f147940298318c7ebf8b87e82b6b14c3
matheustkaczyk <matheus111904@hotmail.com>
2022-09-05 17:51:34 -0300

    feat: criando dto de criacao de empresa

--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -1,1 +1,21 @@
-export class CreateCompanyDto {}
+import { IsNotEmpty, IsString, Matches, IsNumber } from "class-validator";
+
+export class CreateCompanyDto {
+  @IsNotEmpty()
+  @IsString()
+  nome: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Matches(/^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/)
+  cnpj: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @Matches(/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/)
+  data_fundacao: Date;
+
+  @IsNotEmpty()
+  @IsNumber()
+  valor_hora: number;
+}
