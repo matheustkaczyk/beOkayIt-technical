4ed95925e8949938de0880f7a41e9f74a66a7b23
matheustkaczyk <matheus111904@hotmail.com>
2022-09-05 14:50:12 -0300

    feat: configurando connection com database mysql usando typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,23 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CompanyModule } from './company/company.module';
 import { HolidayModule } from './holiday/holiday.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [CompanyModule, HolidayModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CompanyModule,
+    HolidayModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.host,
+      port: Number(process.env.port),
+      username: process.env.user,
+      password: process.env.password,
+      database: process.env.database,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
