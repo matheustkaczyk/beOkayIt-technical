a504f38679d44e3e161d3eba8286304426cb9d73
matheustkaczyk <matheus111904@hotmail.com>
2022-09-05 17:52:46 -0300

    refact: colocando regex em variaveis para deixar mais legivel

--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -1,5 +1,8 @@
 import { IsNotEmpty, IsString, Matches, IsNumber } from "class-validator";
 
+const CNPJ_REGEX = /^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/;
+const DATE_REGEX = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
+
 export class CreateCompanyDto {
   @IsNotEmpty()
   @IsString()
@@ -7,12 +10,12 @@ export class CreateCompanyDto {
 
   @IsNotEmpty()
   @IsString()
-  @Matches(/^\d{2}\.\d{3}\.\d{3}\/\d{4}\-\d{2}$/)
+  @Matches(CNPJ_REGEX)
   cnpj: string;
 
   @IsNotEmpty()
   @IsString()
-  @Matches(/^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/)
+  @Matches(DATE_REGEX)
   data_fundacao: Date;
 
   @IsNotEmpty()
