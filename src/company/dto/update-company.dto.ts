d3d1f2b0c71eb6ed4c1b3b85bae4187a192b4d75
matheustkaczyk <matheus111904@hotmail.com>
2022-09-06 10:40:45 -0300

    feat: adiciona dto de atualizacao de empresa

--- src/company/dto/update-company.dto.ts
+++ src/company/dto/update-company.dto.ts
@@ -1,4 +1,13 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateCompanyDto } from './create-company.dto';
+import { IsNotEmpty, IsString, Matches } from "class-validator";
 
-export class UpdateCompanyDto extends PartialType(CreateCompanyDto) {}
+const VALUE_REGEX = /^[0-9]*(\.[0-9]{0,2})?$/
+
+export class UpdateCompanyDto {
+  @IsNotEmpty()
+  @IsString()
+  nome: string;
+
+  @IsNotEmpty()
+  @Matches(VALUE_REGEX, { message: "Valor deve ser positivo com até duas casas decimais" })
+  valor_hora: number;
+}
