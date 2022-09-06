dd01b4f5c911dcd5cff19bd985a5ebee083d5287
matheustkaczyk <matheus111904@hotmail.com>
2022-09-06 11:20:05 -0300

    feat: criando arquivo de seed, colocando objeto de informacoes

---
+++ src/database/seeds/holiday-create.seed.ts
@@ -0,0 +1,9 @@
+import { Connection } from "typeorm";
+import { Seeder, Factory } from "typeorm-seeding";
+// import { Holiday } from "../holiday.entity";
+
+export class CreateHolidaysSeed implements Seeder {
+  public async run(factory: Factory, connection: Connection): Promise<any> {
+    
+  }
+}
---
+++ src/database/seeds/holiday-infos.ts
@@ -0,0 +1,39 @@
+export const holidays =
+[
+  {
+    "data": "01/01/2022",
+    "nome": "Ano Novo",
+  },
+  {
+    "data": "15/04/2022",
+    "nome": "Sexta-Feira Santa",
+  },
+  {
+    "data": "21/04/2022",
+    "nome": "Dia de Tiradentes",
+  },
+  {
+    "data": "01/05/2022",
+    "nome": "Dia do Trabalho",
+  },
+  {
+    "data": "07/09/2022",
+    "nome": "Independência do Brasil",
+  },
+  {
+    "data": "12/10/2022",
+    "nome": "Nossa Senhora Aparecida",
+  },
+  {
+    "data": "02/11/2022",
+    "nome": "Dia de Finados",
+  },
+  {
+    "data": "15/11/2022",
+    "nome": "Proclamação da República",
+  },
+  {
+    "data": "25/12/2022",
+    "nome": "Natal",
+  }
+]
